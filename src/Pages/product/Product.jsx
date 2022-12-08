b5cc8e03d5e3b6af609a0a62f5e35de516f0e909
piyush-agrawal6 <agrawaljoy1@gmail.com>
2022-12-08 20:50:16 +0530

    updated

--- src/Pages/product/Product.jsx
+++ src/Pages/product/Product.jsx
@@ -1,7 +1,87 @@
 import React from "react";
 import "./Product.css";
+import { Select } from "antd";
+
 const Product = () => {
-  return <div>Product</div>;
+  const sortOptions = [
+    {
+      label: "Better Discount",
+      value: "discount",
+    },
+    {
+      label: "Customer Ratings",
+      value: "rating",
+    },
+    {
+      label: "Price low to high",
+      value: "asc",
+    },
+    {
+      label: "Price high to low",
+      value: "desc",
+    },
+  ];
+  const sizeOptions = [
+    {
+      label: "XL",
+      value: "discount",
+    },
+    {
+      label: "SM",
+      value: "rating",
+    },
+    {
+      label: "MD",
+      value: "asc",
+    },
+    {
+      label: "XS",
+      value: "desc",
+    },
+  ];
+  return (
+    <div className="productCon">
+      <div className="proContainer">
+        <p className="proNavigation">
+          <span>Home /</span> Men
+        </p>
+        <p className="proCount">
+          Products - <span>133536 items</span>
+        </p>
+        <div className="proSort">
+          <p>FILTERS</p>
+          <div>
+            <Select
+              size="large"
+              placeholder="Sort By"
+              style={{
+                width: 200,
+                border: "1px solid gray",
+                color: "black",
+                borderRadius: "8px",
+                outline: "none",
+              }}
+              options={sortOptions}
+            />
+            <br />
+            <Select
+              mode="multiple"
+              size="large"
+              placeholder="Please Select Size"
+              style={{
+                width: 250,
+                border: "1px solid gray",
+                color: "black",
+                borderRadius: "8px",
+                outline: "none",
+              }}
+              options={sizeOptions}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Product;
