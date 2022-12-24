34df45ea630e8eb2f9644fc92099a48131d11084
piyush-agrawal6 <agrawaljoy1@gmail.com>
2022-12-24 12:30:55 +0530

    product page  addded

--- src/Pages/product/SingleProduct.jsx
+++ src/Pages/product/SingleProduct.jsx
@@ -2,11 +2,15 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getProductDetails } from "../../Redux/product/action";
-
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper";
+import "./singleproduct.css";
 const SingleProduct = () => {
   let { id } = useParams();
   const dispatch = useDispatch();
-  const data = useSelector((store) => store.products.product)
+  const data = useSelector((store) => store.products.product);
   let image = [];
   if (data) {
     for (let key in data.images) {
@@ -18,7 +22,26 @@ const SingleProduct = () => {
   useEffect(() => {
     dispatch(getProductDetails(id));
   }, [id, dispatch]);
-  return <div>SingleProduct</div>;
+  return (
+    <div className="singleProComponent">
+      <div className="singleProNavigation">Home / Men / Product</div>
+      <div className="singlePro">
+        <div className="singleProGallery">
+          <Swiper pagination={true} modules={[Pagination]} className="mySwiper">
+            {image?.map((e) => {
+              return (
+                <SwiperSlide className="swipeImage">
+                  <img src={e} alt="images" />
+                </SwiperSlide>
+              );
+            })}
+          </Swiper>
+        </div>
+        <div className="singleProDetails"></div>
+      </div>
+      <div className="singleProReviews"></div>
+    </div>
+  );
 };
 
 export default SingleProduct;
